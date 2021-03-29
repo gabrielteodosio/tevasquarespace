5cafc5b4edac76df05ca4eb40f58e112fd4d13a7
Gabriel Teodosio <gabrielteodosio@tevaindices.com.br>
2021-03-29 16:14:17 -0300

    removed plotly and added d3 to csv data reading

--- scripts/funds.js
+++ scripts/funds.js
@@ -1,5 +1,3 @@
-Plotly.setPlotConfig({ locale: "pt-BR" });
-
 const csvsUrls = {
   higherRelevance:
     "https://raw.githubusercontent.com/gabrielteodosio/tevasquarespace/master/csv/indice_debentures_alta_liquidez__50%25_de_negociacao_nos_ultimos_dois_meses_e_5mm_de_negociacao_nos_ultimos_dois_meses__precificacao_anbima_v0_94/ativos_com_maior_relevancia.csv",
@@ -47,103 +45,31 @@ const CssSpinner = Vue.component("loading-css", {
 Vue.config.devtools = true;
 const app = new Vue({
   el: "#funds-app",
-  data: () => {
-    const selectorOptions = {
-      buttons: [
-        {
-          step: "week",
-          stepmode: "backward",
-          count: 1,
-          label: "1S",
-        },
-        {
-          step: "month",
-          stepmode: "backward",
-          count: 1,
-          label: "1M",
-        },
-        {
-          step: "month",
-          stepmode: "backward",
-          count: 3,
-          label: "3M",
-        },
-        {
-          step: "year",
-          stepmode: "backward",
-          count: 1,
-          label: "1A",
-        },
-        {
-          step: "year",
-          stepmode: "todate",
-          count: 1,
-          label: "YTD",
-        },
-        {
-          step: "all",
-          label: "MAX"
-        },
-      ],
-    };
-
-    return {
-      topTen: [],
-      convexity: {},
-      duration: {},
-      modifiedDuration: {},
-      yieldToMaturity: {},
-      turnOverLTM: null,
-      ticksNumber: {},
-      repactuationMedia: {},
-      anualReturn: [],
-      loadingMetrics: true,
-      standardDeviation: [],
-      anualReturn: [],
-      sharpeIndex: [],
-      indexExposition: [],
-      quotesChart: {
-        uuid: "quotes-chart",
-        traces: [],
-        layout: {
-          hovermode: "x unified",
-          hoverlabel: {
-            bgcolor: "#000",
-            font: {
-              family: "halyard-display, Arial, sans-serif",
-              color: "#fff",
-            },
-          },
-          showlegend: true,
-          legend: {
-            orientation: "h",
-            x: 0.4,
-            y: 1.2,
-          },
-          xaxis: {
-            type: "date",
-            showline: true,
-            autorange: true,
-            showgrid: false,
-            ticks: 'outside',
-            // tickformat: "%d/%m/%Y",
-            rangeslider: { margintop: 50 },
-            rangeselector: selectorOptions,
-            title: { text: "Data de referência" },
-          },
-          yaxis: {
-            zeroline: true,
-            title: { text: "Cotação do índice" },
-          },
-        },
-        indexDesc:
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eu lectus tincidunt, sodales diam in, dignissim lorem. Maecenas molestie et est at maximus. Sed non bibendum urna. Proin elementum justo massa, vel congue magna eleifend at. Curabitur accumsan nulla sit amet diam interdum accumsan. Nulla non arcu id lacus aliquet dapibus in ut massa. Aliquam fringilla sapien nunc, id lacinia enim aliquet non. Fusce pretium at nunc ac placerat.",
-      },
-    };
-  },
+  data: () => ({
+    topTen: [],
+    convexity: {},
+    duration: {},
+    modifiedDuration: {},
+    yieldToMaturity: {},
+    turnOverLTM: null,
+    ticksNumber: {},
+    repactuationMedia: {},
+    anualReturn: [],
+    loadingMetrics: true,
+    standardDeviation: [],
+    anualReturn: [],
+    sharpeIndex: [],
+    indexExposition: [],
+    quotesChart: {
+      uuid: "quotes-chart",
+      traces: [],
+      indexDesc:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eu lectus tincidunt, sodales diam in, dignissim lorem. Maecenas molestie et est at maximus. Sed non bibendum urna. Proin elementum justo massa, vel congue magna eleifend at. Curabitur accumsan nulla sit amet diam interdum accumsan. Nulla non arcu id lacus aliquet dapibus in ut massa. Aliquam fringilla sapien nunc, id lacinia enim aliquet non. Fusce pretium at nunc ac placerat.",
+    },
+  }),
   created() {
-    this.processTWHL = processTickersWithHighRelevance.bind(this);
     this.processQuotes = processQuotes.bind(this);
+    this.processTWHL = processTickersWithHighRelevance.bind(this);
     this.processStandardDeviation = processStandardDeviation.bind(this);
     this.processSharpeIndex = processSharpeIndex.bind(this);
     this.processConvexity = processConvexity.bind(this);
@@ -156,14 +82,12 @@ const app = new Vue({
     this.processTicksNumber = processTicksNumber.bind(this);
     this.processRepactuationMedia = processRepactuationMedia.bind(this);
 
-    this.numberToPercentalDecimalsDigits = numberToPercentalDecimalsDigits.bind(
-      this
-    );
+    this.numberToPercentalDecimalsDigits = numberToPercentalDecimalsDigits.bind(this);
     this.numberToDecimalsDigits = numberToDecimalsDigits.bind(this);
   },
   mounted() {
-    this.processTWHL();
     this.processQuotes();
+    this.processTWHL();
     this.processStandardDeviation();
     this.processSharpeIndex();
     this.processConvexity();
@@ -175,22 +99,13 @@ const app = new Vue({
     this.processIndexExposition();
     this.processTicksNumber();
     this.processRepactuationMedia();
-
-    const chartConfig = {
-      locale: "pt-BR",
-      responsive: true,
-      displayModeBar: false,
-    };
-
-    const { uuid, traces, layout } = this.quotesChart;
-    Plotly.newPlot(uuid, traces, layout, chartConfig);
   },
 });
 
 // read files functions
 
 function processTickersWithHighRelevance() {
-  const processFile = (_err, rows) => {
+  const processFile = (rows) => {
     let highestDate = "1900-01-01";
     const data = rows.filter((row) => {
       highestDate =
@@ -213,85 +128,77 @@ function processTickersWithHighRelevance() {
     this.loadingMetrics = false;
   };
 
-  Plotly.d3.csv(csvsUrls.higherRelevance, processFile);
+  d3.csv(csvsUrls.higherRelevance).then(processFile);
 }
 
 function processQuotes() {
-  const processFile = (_err, rows) => {
+  const processData = (rows) => {
     const unpack = (rows, key) => rows.map((row) => row[key]);
 
     const trace = {
-      mode: "lines",
-      type: "scatter",
       line: { color: "rgb(53,149,233)" },
       name: "Índice Debêntures DI (Idex)",
 
       y: unpack(rows, "Valor do índice"),
       x: unpack(rows, "Data de referência"),
-
-      showlegend: false,
-      hovertemplate: "%{y:6,.2f}",
     };
 
     this.quotesChart.traces = [trace];
-
-    const { uuid, traces, layout } = this.quotesChart;
-    Plotly.react(uuid, traces, layout);
   };
 
-  Plotly.d3.csv(csvsUrls.quotes, processFile);
+  d3.csv(csvsUrls.quotes).then(processData);
 }
 
 function processStandardDeviation() {
-  const processFile = (_err, rows) => (this.standardDeviation = rows);
-  Plotly.d3.csv(csvsUrls.standardDeviation, processFile);
+  const processFile = (rows) => (this.standardDeviation = rows);
+  d3.csv(csvsUrls.standardDeviation).then(processFile);
 }
 
 function processSharpeIndex() {
-  const processFile = (_err, rows) => (this.sharpeIndex = rows);
-  Plotly.d3.csv(csvsUrls.sharpeIndex, processFile);
+  const processFile = (rows) => (this.sharpeIndex = rows);
+  d3.csv(csvsUrls.sharpeIndex).then(processFile);
 }
 
 function processSharpeIndex() {
-  const processFile = (_err, rows) => (this.sharpeIndex = rows);
-  Plotly.d3.csv(csvsUrls.sharpeIndex, processFile);
+  const processFile = (rows) => (this.sharpeIndex = rows);
+  d3.csv(csvsUrls.sharpeIndex).then(processFile);
 }
 
 function processConvexity() {
-  const processFile = (_err, rows) => (this.convexity = rows[rows.length - 1]);
-  Plotly.d3.csv(csvsUrls.convexity, processFile);
+  const processFile = (rows) => (this.convexity = rows[rows.length - 1]);
+  d3.csv(csvsUrls.convexity).then(processFile);
 }
 
 function processDuration() {
-  const processFile = (_err, rows) => (this.duration = rows[rows.length - 1]);
-  Plotly.d3.csv(csvsUrls.duration, processFile);
+  const processFile = (rows) => (this.duration = rows[rows.length - 1]);
+  d3.csv(csvsUrls.duration).then(processFile);
 }
 
 function processModifiedDuration() {
-  const processFile = (_err, rows) =>
+  const processFile = (rows) =>
     (this.modifiedDuration = rows[rows.length - 1]);
-  Plotly.d3.csv(csvsUrls.modifiedDuration, processFile);
+  d3.csv(csvsUrls.modifiedDuration).then(processFile);
 }
 
 function processYieldToMaturity() {
-  const processFile = (_err, rows) =>
+  const processFile = (rows) =>
     (this.yieldToMaturity = rows[rows.length - 1]);
-  Plotly.d3.csv(csvsUrls.yieldToMaturity, processFile);
+  d3.csv(csvsUrls.yieldToMaturity).then(processFile);
 }
 
 function processAnualReturn() {
-  const processFile = (_err, rows) => (this.anualReturn = rows);
-  Plotly.d3.csv(csvsUrls.anualReturn, processFile);
+  const processFile = (rows) => (this.anualReturn = rows);
+  d3.csv(csvsUrls.anualReturn).then(processFile);
 }
 
 function processTurnOverLTM() {
-  const processFile = (_err, rows) =>
+  const processFile = (rows) =>
     (this.turnOverLTM = rows[rows.length - 1]["Turnover"]);
-  Plotly.d3.csv(csvsUrls.turnOverLTM, processFile);
+  d3.csv(csvsUrls.turnOverLTM).then(processFile);
 }
 
 function processIndexExposition() {
-  const processFile = (_err, rows) => {
+  const processFile = (rows) => {
     let highestDate = "1900-01-01";
     const data = rows.filter((row) => {
       highestDate =
@@ -305,24 +212,23 @@ function processIndexExposition() {
       (data) => data["Data de referência"] == highestDate
     );
   };
-  Plotly.d3.csv(csvsUrls.indexExposition, processFile);
+  d3.csv(csvsUrls.indexExposition).then(processFile);
 }
 
 function processTicksNumber() {
-  const processFile = (_err, rows) => {
+  const processFile = (rows) => {
     const sortedData = multiSort(rows, { "Data de referência": "desc" });
     this.ticksNumber = sortedData[0];
   };
-  Plotly.d3.csv(csvsUrls.ticksNumber, processFile);
+  d3.csv(csvsUrls.ticksNumber).then(processFile);
 }
 
 function processRepactuationMedia() {
-  const processFile = (_err, rows) => {
+  const processFile = (rows) => {
     const sortedData = multiSort(rows, { "Data de referência": "desc" });
-    console.log({ rows, sortedData });
     this.repactuationMedia = sortedData[0];
   };
-  Plotly.d3.csv(csvsUrls.repactuationMedia, processFile);
+  d3.csv(csvsUrls.repactuationMedia).then(processFile);
 }
 
 // utlity functions
