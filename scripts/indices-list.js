eec05ddc1b6bad3c23e5c1c0d65ae3fcb4ec4469
Gabriel Teodosio <gabrielteodosio@tevaindices.com.br>
2021-05-03 12:04:48 -0300

    filters list scripts update

--- scripts/indices-list.js
+++ scripts/indices-list.js
@@ -57,6 +57,65 @@ document.getElementById("tesouro-family-filter").addEventListener("change", (e) 
   }
 });
 
+document.getElementById("esg-filter-text").addEventListener(
+  "keyup",
+  debounce(
+    (e) => {
+      const filter = e.target.value.toUpperCase();
+      const list = document.getElementById("esg-indices-list").children;
+      let familyFilter = document.getElementById("esg-family-filter").value.toUpperCase();
+
+      for (let i = 0; i < list.length; i++) {
+        let name = list[i]
+          .getElementsByClassName("index-title")[0]
+          .innerHTML.toUpperCase();
+
+        const condition = name.indexOf(filter) >= 0 &&
+                          (familyFilter === "ALL" ? true : name.indexOf(familyFilter) >= 0);        
+
+        if (condition) list[i].style.display = "flex";
+        else list[i].style.display = "none";
+      }
+    },
+    250,
+    false
+  )
+);
+
+document.getElementById("esg-family-filter").addEventListener("change", (e) => {
+  const filter = e.target.value.toUpperCase();
+  const list = document.getElementById("esg-indices-list").children;
+  const textFilter = document.getElementById("esg-filter-text").value.toUpperCase();
+
+  if (filter === "ALL") {
+    for (let i = 0; i < list.length; i++) {
+      let name = list[i]
+          .getElementsByClassName("index-title")[0]
+          .innerHTML.toUpperCase();
+
+      const condition = name.indexOf(textFilter) >= 0 &&
+                        (filter === "ALL" ? true : name.indexOf(filter) >= 0);        
+
+      if (condition) list[i].style.display = "flex";
+      else list[i].style.display = "none";
+    }
+
+    return;
+  }
+
+  for (let i = 0; i < list.length; i++) {
+    let name = list[i]
+      .getElementsByClassName("index-title")[0]
+      .innerHTML.toUpperCase();
+
+    const condition = name.indexOf(filter) >= 0 &&
+                      name.indexOf(textFilter) >= 0;
+
+    if (condition) list[i].style.display = "flex";
+    else list[i].style.display = "none";
+  }
+});
+
 document.getElementById("imobiliarios-indices-filter-text").addEventListener(
   "keyup",
   debounce(
@@ -86,6 +145,65 @@ document.getElementById("imobiliarios-family-filter").addEventListener("change",
   const filter = e.target.value.toUpperCase();
   const list = document.getElementById("imobiliarios-indices-list").children;
   const textFilter = document.getElementById("imobiliarios-indices-filter-text").value.toUpperCase();
+
+  if (filter === "ALL") {
+    for (let i = 0; i < list.length; i++) {
+      let name = list[i]
+          .getElementsByClassName("index-title")[0]
+          .innerHTML.toUpperCase();
+
+      const condition = name.indexOf(textFilter) >= 0 &&
+                        (filter === "ALL" ? true : name.indexOf(filter) >= 0);        
+
+      if (condition) list[i].style.display = "flex";
+      else list[i].style.display = "none";
+    }
+
+    return;
+  }
+
+  for (let i = 0; i < list.length; i++) {
+    let name = list[i]
+      .getElementsByClassName("index-title")[0]
+      .innerHTML.toUpperCase();
+
+    const condition = name.indexOf(filter) >= 0 &&
+                      name.indexOf(textFilter) >= 0;
+
+    if (condition) list[i].style.display = "flex";
+    else list[i].style.display = "none";
+  }
+});
+
+document.getElementById("acoes-filter-text").addEventListener(
+  "keyup",
+  debounce(
+    (e) => {
+      const filter = e.target.value.toUpperCase();
+      const list = document.getElementById("acoes-indices-list").children;
+      let familyFilter = document.getElementById("acoes-family-filter").value.toUpperCase();
+
+      for (let i = 0; i < list.length; i++) {
+        let name = list[i]
+          .getElementsByClassName("index-title")[0]
+          .innerHTML.toUpperCase();
+
+        const condition = name.indexOf(filter) >= 0 &&
+                          (familyFilter === "ALL" ? true : name.indexOf(familyFilter) >= 0);        
+
+        if (condition) list[i].style.display = "flex";
+        else list[i].style.display = "none";
+      }
+    },
+    250,
+    false
+  )
+);
+
+document.getElementById("acoes-family-filter").addEventListener("change", (e) => {
+  const filter = e.target.value.toUpperCase();
+  const list = document.getElementById("acoes-indices-list").children;
+  const textFilter = document.getElementById("acoes-filter-text").value.toUpperCase();
 
   if (filter === "ALL") {
     for (let i = 0; i < list.length; i++) {
