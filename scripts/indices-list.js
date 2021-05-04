9a9c371944e11d2ab98dd7e6ed55c1fac1710d18
Gabriel Teodosio <gabrielteodosio@tevaindices.com.br>
2021-05-04 09:56:27 -0300

    indices list and filter

--- scripts/indices-list.js
+++ scripts/indices-list.js
@@ -1,18 +1,85 @@
+const indices = {
+  fiis: {
+    amplos: [
+      "índice de fundos imobiliários",
+      "índice de fundos imobiliários liquidez",
+      "índice de fundos imobiliários top 40",
+      "índice de fundos imobiliários top 30",
+      "índice de fundos imobiliários top 15",
+    ],
+    "tipo de fundo": [
+      "índice de fundos imobiliários de papel",
+      "índice de fundos imobiliários de tijolo",
+    ],
+    segmento: [
+      "índice de fundos imobiliários de logística",
+      "índice de fundos imobiliários de lages corporativas",
+      "índice de fundos imobiliários de shoppings",
+      "índice de fundos imobiliários segmento híbrido",
+    ],
+    mandato: [
+      "índice de fundos imobiliários mandato renda",
+      "índice de fundos imobiliários mandato híbrido",
+    ],
+    fatores: [
+      "índice de fundos imobiliários rendimento",
+      "índice de fundos imobiliários valor",
+    ],
+    indexadores: [
+      "índice de fundos imobiliários tijolo ipca",
+      "índice de fundos imobiliários tijolo igpm",
+    ],
+  },
+  acoes: {
+    amplo: ["índice de ações amplo"],
+    esg: [
+      "índice de ações esg clean",
+      "índice de ações esg diversidade",
+    ],
+    setores: [
+      "índice de ações tecnologia - listadas no BR e US",
+      "índice de ações tecnologia",
+      "índice de ações saúde",
+      "índice de ações bens de consumo e varejo",
+      "índice de ações energia",
+      "índice de ações imobiliário",
+      "índice de ações commodities",
+      "índice de ações financeiro",
+      "índice de ações industrial",
+      "índice de ações comércio",
+      "índice de ações viagem e turismo",
+      "índice de ações infraestrutura",
+      "índice de ações serviços",
+      "índice de ações construção civil",
+      "índice de ações agricultura",
+    ],
+    tematico: [
+      "índice de ações excl. empresas estatais",
+      "índice de ações empresas públicas",
+      "índice de ações exportação",
+      "índice de ações inflação",
+    ]
+  },
+};
+
 document.getElementById("tesouro-filter-text").addEventListener(
   "keyup",
   debounce(
     (e) => {
       const filter = e.target.value.toUpperCase();
       const list = document.getElementById("tesouro-indices-list").children;
-      let familyFilter = document.getElementById("tesouro-family-filter").value.toUpperCase();
+      let familyFilter = document
+        .getElementById("tesouro-family-filter")
+        .value.toUpperCase();
 
       for (let i = 0; i < list.length; i++) {
         let name = list[i]
           .getElementsByClassName("index-title")[0]
           .innerHTML.toUpperCase();
 
-        const condition = name.indexOf(filter) >= 0 &&
-                          (familyFilter === "ALL" ? true : name.indexOf(familyFilter) >= 0);        
+        const condition =
+          name.indexOf(filter) >= 0 &&
+          (familyFilter === "ALL" ? true : name.indexOf(familyFilter) >= 0);
 
         if (condition) list[i].style.display = "flex";
         else list[i].style.display = "none";
@@ -23,39 +90,44 @@ document.getElementById("tesouro-filter-text").addEventListener(
   )
 );
 
-document.getElementById("tesouro-family-filter").addEventListener("change", (e) => {
-  const filter = e.target.value.toUpperCase();
-  const list = document.getElementById("tesouro-indices-list").children;
-  const textFilter = document.getElementById("tesouro-filter-text").value.toUpperCase();
+document
+  .getElementById("tesouro-family-filter")
+  .addEventListener("change", (e) => {
+    const filter = e.target.value.toUpperCase();
+    const list = document.getElementById("tesouro-indices-list").children;
+    const textFilter = document
+      .getElementById("tesouro-filter-text")
+      .value.toUpperCase();
 
-  if (filter === "ALL") {
-    for (let i = 0; i < list.length; i++) {
-      let name = list[i]
+    if (filter === "ALL") {
+      for (let i = 0; i < list.length; i++) {
+        let name = list[i]
           .getElementsByClassName("index-title")[0]
           .innerHTML.toUpperCase();
 
-      const condition = name.indexOf(textFilter) >= 0 &&
-                        (filter === "ALL" ? true : name.indexOf(filter) >= 0);        
+        const condition =
+          name.indexOf(textFilter) >= 0 &&
+          (filter === "ALL" ? true : name.indexOf(filter) >= 0);
+
+        if (condition) list[i].style.display = "flex";
+        else list[i].style.display = "none";
+      }
+
+      return;
+    }
+
+    for (let i = 0; i < list.length; i++) {
+      let name = list[i]
+        .getElementsByClassName("index-title")[0]
+        .innerHTML.toUpperCase();
+
+      const condition =
+        name.indexOf(filter) >= 0 && name.indexOf(textFilter) >= 0;
 
       if (condition) list[i].style.display = "flex";
       else list[i].style.display = "none";
     }
-
-    return;
-  }
-
-  for (let i = 0; i < list.length; i++) {
-    let name = list[i]
-      .getElementsByClassName("index-title")[0]
-      .innerHTML.toUpperCase();
-
-    const condition = name.indexOf(filter) >= 0 &&
-                      name.indexOf(textFilter) >= 0;
-
-    if (condition) list[i].style.display = "flex";
-    else list[i].style.display = "none";
-  }
-});
+  });
 
 document.getElementById("esg-filter-text").addEventListener(
   "keyup",
@@ -63,15 +135,18 @@ document.getElementById("esg-filter-text").addEventListener(
     (e) => {
       const filter = e.target.value.toUpperCase();
       const list = document.getElementById("esg-indices-list").children;
-      let familyFilter = document.getElementById("esg-family-filter").value.toUpperCase();
+      let familyFilter = document
+        .getElementById("esg-family-filter")
+        .value.toUpperCase();
 
       for (let i = 0; i < list.length; i++) {
         let name = list[i]
           .getElementsByClassName("index-title")[0]
           .innerHTML.toUpperCase();
 
-        const condition = name.indexOf(filter) >= 0 &&
-                          (familyFilter === "ALL" ? true : name.indexOf(familyFilter) >= 0);        
+        const condition =
+          name.indexOf(filter) >= 0 &&
+          (familyFilter === "ALL" ? true : name.indexOf(familyFilter) >= 0);
 
         if (condition) list[i].style.display = "flex";
         else list[i].style.display = "none";
@@ -85,16 +160,19 @@ document.getElementById("esg-filter-text").addEventListener(
 document.getElementById("esg-family-filter").addEventListener("change", (e) => {
   const filter = e.target.value.toUpperCase();
   const list = document.getElementById("esg-indices-list").children;
-  const textFilter = document.getElementById("esg-filter-text").value.toUpperCase();
+  const textFilter = document
+    .getElementById("esg-filter-text")
+    .value.toUpperCase();
 
   if (filter === "ALL") {
     for (let i = 0; i < list.length; i++) {
       let name = list[i]
-          .getElementsByClassName("index-title")[0]
-          .innerHTML.toUpperCase();
+        .getElementsByClassName("index-title")[0]
+        .innerHTML.toUpperCase();
 
-      const condition = name.indexOf(textFilter) >= 0 &&
-                        (filter === "ALL" ? true : name.indexOf(filter) >= 0);        
+      const condition =
+        name.indexOf(textFilter) >= 0 &&
+        (filter === "ALL" ? true : name.indexOf(filter) >= 0);
 
       if (condition) list[i].style.display = "flex";
       else list[i].style.display = "none";
@@ -108,8 +186,8 @@ document.getElementById("esg-family-filter").addEventListener("change", (e) => {
       .getElementsByClassName("index-title")[0]
       .innerHTML.toUpperCase();
 
-    const condition = name.indexOf(filter) >= 0 &&
-                      name.indexOf(textFilter) >= 0;
+    const condition =
+      name.indexOf(filter) >= 0 && name.indexOf(textFilter) >= 0;
 
     if (condition) list[i].style.display = "flex";
     else list[i].style.display = "none";
@@ -121,16 +199,22 @@ document.getElementById("imobiliarios-indices-filter-text").addEventListener(
   debounce(
     (e) => {
       const filter = e.target.value.toUpperCase();
-      const list = document.getElementById("imobiliarios-indices-list").children;
-      let familyFilter = document.getElementById("imobiliarios-family-filter").value.toUpperCase();
+      const list = document.getElementById("imobiliarios-indices-list")
+        .children;
+      const familyFilter = document
+        .getElementById("imobiliarios-family-filter")
+        .value.toUpperCase();
 
       for (let i = 0; i < list.length; i++) {
         let name = list[i]
           .getElementsByClassName("index-title")[0]
           .innerHTML.toUpperCase();
 
-        const condition = name.indexOf(filter) >= 0 &&
-                          (familyFilter === "ALL" ? true : name.indexOf(familyFilter) >= 0);        
+        const condition =
+          name.indexOf(filter) >= 0 &&
+          (familyFilter === "ALL"
+            ? true
+            : indices.fiis[familyFilter.toLowerCase()]?.includes(name.toLowerCase()));
 
         if (condition) list[i].style.display = "flex";
         else list[i].style.display = "none";
@@ -141,39 +225,43 @@ document.getElementById("imobiliarios-indices-filter-text").addEventListener(
   )
 );
 
-document.getElementById("imobiliarios-family-filter").addEventListener("change", (e) => {
-  const filter = e.target.value.toUpperCase();
-  const list = document.getElementById("imobiliarios-indices-list").children;
-  const textFilter = document.getElementById("imobiliarios-indices-filter-text").value.toUpperCase();
+document
+  .getElementById("imobiliarios-family-filter")
+  .addEventListener("change", (e) => {
+    const filter = e.target.value.toUpperCase();
+    const list = document.getElementById("imobiliarios-indices-list").children;
+    const textFilter = document
+      .getElementById("imobiliarios-indices-filter-text")
+      .value.toUpperCase();
 
-  if (filter === "ALL") {
-    for (let i = 0; i < list.length; i++) {
-      let name = list[i]
+    if (filter === "ALL") {
+      for (let i = 0; i < list.length; i++) {
+        let name = list[i]
           .getElementsByClassName("index-title")[0]
           .innerHTML.toUpperCase();
 
-      const condition = name.indexOf(textFilter) >= 0 &&
-                        (filter === "ALL" ? true : name.indexOf(filter) >= 0);        
+        const condition = name.toLowerCase().indexOf(textFilter.toLowerCase()) >= 0;
+
+        if (condition) list[i].style.display = "flex";
+        else list[i].style.display = "none";
+      }
+
+      return;
+    }
+
+    for (let i = 0; i < list.length; i++) {
+      let name = list[i]
+        .getElementsByClassName("index-title")[0]
+        .innerHTML.toUpperCase();
+
+      const condition =
+        name.toLowerCase().indexOf(textFilter.toLowerCase()) >= 0 &&
+        indices.fiis[filter.toLowerCase()]?.includes(name.toLowerCase());
 
       if (condition) list[i].style.display = "flex";
       else list[i].style.display = "none";
     }
-
-    return;
-  }
-
-  for (let i = 0; i < list.length; i++) {
-    let name = list[i]
-      .getElementsByClassName("index-title")[0]
-      .innerHTML.toUpperCase();
-
-    const condition = name.indexOf(filter) >= 0 &&
-                      name.indexOf(textFilter) >= 0;
-
-    if (condition) list[i].style.display = "flex";
-    else list[i].style.display = "none";
-  }
-});
+  });
 
 document.getElementById("acoes-filter-text").addEventListener(
   "keyup",
@@ -181,15 +269,18 @@ document.getElementById("acoes-filter-text").addEventListener(
     (e) => {
       const filter = e.target.value.toUpperCase();
       const list = document.getElementById("acoes-indices-list").children;
-      let familyFilter = document.getElementById("acoes-family-filter").value.toUpperCase();
+      const familyFilter = document
+        .getElementById("acoes-family-filter")
+        .value.toUpperCase();
 
       for (let i = 0; i < list.length; i++) {
         let name = list[i]
           .getElementsByClassName("index-title")[0]
           .innerHTML.toUpperCase();
 
-        const condition = name.indexOf(filter) >= 0 &&
-                          (familyFilter === "ALL" ? true : name.indexOf(familyFilter) >= 0);        
+        const condition =
+          name.indexOf(filter) >= 0 &&
+          (familyFilter === "ALL" ? true : name.indexOf(familyFilter) >= 0);
 
         if (condition) list[i].style.display = "flex";
         else list[i].style.display = "none";
@@ -200,39 +291,41 @@ document.getElementById("acoes-filter-text").addEventListener(
   )
 );
 
-document.getElementById("acoes-family-filter").addEventListener("change", (e) => {
-  const filter = e.target.value.toUpperCase();
-  const list = document.getElementById("acoes-indices-list").children;
-  const textFilter = document.getElementById("acoes-filter-text").value.toUpperCase();
+document
+  .getElementById("acoes-family-filter")
+  .addEventListener("change", (e) => {
+    const filter = e.target.value.toUpperCase();
+    const list = document.getElementById("acoes-indices-list").children;
+    const textFilter = document.getElementById("acoes-filter-text")
+      .value.toUpperCase();
 
-  if (filter === "ALL") {
-    for (let i = 0; i < list.length; i++) {
-      let name = list[i]
+    if (filter === "ALL") {
+      for (let i = 0; i < list.length; i++) {
+        let name = list[i]
           .getElementsByClassName("index-title")[0]
           .innerHTML.toUpperCase();
 
-      const condition = name.indexOf(textFilter) >= 0 &&
-                        (filter === "ALL" ? true : name.indexOf(filter) >= 0);        
+        const condition = name.toLowerCase().includes(textFilter.toLowerCase());
 
-      if (condition) list[i].style.display = "flex";
-      else list[i].style.display = "none";
+        if (condition) list[i].style.display = "flex";
+        else list[i].style.display = "none";
+      }
+    } else {
+      for (let i = 0; i < list.length; i++) {
+        let name = list[i]
+          .getElementsByClassName("index-title")[0]
+          .innerHTML.toUpperCase();
+  
+        const condition =
+          name.toLowerCase().includes(textFilter.toLowerCase()) &&
+          indices.acoes[filter.toLowerCase()]?.includes(name.toLowerCase());
+  
+        if (condition) list[i].style.display = "flex";
+        else list[i].style.display = "none";
+      }
     }
 
-    return;
-  }
-
-  for (let i = 0; i < list.length; i++) {
-    let name = list[i]
-      .getElementsByClassName("index-title")[0]
-      .innerHTML.toUpperCase();
-
-    const condition = name.indexOf(filter) >= 0 &&
-                      name.indexOf(textFilter) >= 0;
-
-    if (condition) list[i].style.display = "flex";
-    else list[i].style.display = "none";
-  }
-});
+  });
 
 function navigateTo(endpoint) {
   window.location.href = "/" + endpoint;
